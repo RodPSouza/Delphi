51d84301bc260689c4e02f092e9d6d1a8ad99f8d
RodPSouza <rod.p.souza@hotmail.com>
2016-10-26 22:18:19 -0200

    Segunda_Carga
    -Adcionado o Atalho Alt+Zero

--- Form_Principal.pas
+++ Form_Principal.pas
@@ -4,12 +4,18 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs;
+  Dialogs, Grids, DBGrids, StdCtrls;
 
 type
   TForm1 = class(TForm)
+    Edit1: TEdit;
+    DBGrid1: TDBGrid;
+    procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
   private
     { Private declarations }
+
+    procedure WMHotKey(var Msg: TWMHotKey); message WM_HOTKEY;
   public
     { Public declarations }
   end;
@@ -19,6 +25,29 @@ var
 
 implementation
 
+uses Unit2;
+
 {$R *.dfm}
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  //Registrando a HotKey
+    RegisterHotKey(Handle,0, MOD_ALT , VK_NUMPAD0);
+
+end;
+
+procedure TForm1.FormDestroy(Sender: TObject);
+begin
+  //Desresgistrando a HotKey
+  UnregisterHotKey(Handle,0);
+
+end;
+
+procedure TForm1.WMHotKey(var Msg: TWMHotKey);
+begin
+  //Teste Simples
+  Form2.Show;
+
+End;
 
 end.
